6e6d3564dc709842d53f1fb3fe3146ff2645e028
brenopimentell <brenofelipe.pimentel@gmail.com>
2023-02-20 12:09:42 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const secondsEl = document.querySelector("#seconds");
+const milisecondsEl = document.querySelector("#miliseconds");
+const startbot = document.querySelector("#startbot");
+const pausebot = document.querySelector("#pausebot");
+const continuebot = document.querySelector("#continuebot");
+const pausebot = document.querySelector("#pausebot");
+
+let minutes = 0;
+let seconds = 0;
+let miliseconds = 0;
+let isPaused = false;
